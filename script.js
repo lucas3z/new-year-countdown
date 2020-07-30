68bc964f47716c1e4b39a8807a1fed569827b3cc
Lucas <lucaoliveira57@gmail.com>
2020-07-30 16:37:43 -0300

    Added prefixed zero when number is less than ten on countdown

--- script.js
+++ script.js
@@ -25,9 +25,9 @@ function updateCountdown() {
 
   // Add values to DOM
   days.innerHTML = d;
-  hours.innerHTML = h;
-  minutes.innerHTML = m;
-  seconds.innerHTML = s;
+  hours.innerHTML = h < 10 ? '0' + h : h;
+  minutes.innerHTML = m < 10 ? '0' + m : m;
+  seconds.innerHTML = s < 10 ? '0' + s : s;
 }
 
 // Show spinner before countdown
